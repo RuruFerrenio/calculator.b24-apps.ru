c42a426bfcb5123cabd49e30c111a3f6f8829cf6
i.demchenko <@_i@#KPO23KJte>
2026-04-22 20:35:33 +0300

    chore: настройка роутинга для открытия модалок

--- src/pages/widgets/background-handler.vue
+++ src/pages/widgets/background-handler.vue
@@ -238,9 +238,13 @@ function openWorkdayModal(mode: 'start' | 'end'): void {
     width: MODAL_CONFIG.WIDTH,
   }
 
-  BX24.openApplication(parameters, function() {
-    onModalClosed(mode)
-  }, modalSettings)
+  BX24.openApplication({
+        opened: true
+      },
+      function()
+      {
+        alert('Application closed!');
+  })
 }
 
 // Обработчик закрытия модального окна
