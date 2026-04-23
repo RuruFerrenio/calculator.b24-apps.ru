0e3ec6b9f9fe87c2afa45c7100bb9875084acf44
i.demchenko <@_i@#KPO23KJte>
2026-04-23 18:02:50 +0300

    chore: замена иконки в модалке

--- src/components/SolutionsSlider.vue
+++ src/components/SolutionsSlider.vue
@@ -93,12 +93,11 @@
                         <B24Button
                             :variant="solution.installed ? 'outline' : 'primary'"
                             @click="handleReview"
+                            color="air-primary"
                             class="w-full justify-center"
                         >
                           {{ solution.installed ? 'Открыть' : 'Подробнее' }}
-                          <svg class="w-3.5 h-3.5 ml-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                          </svg>
+                          <ArrowToTheRightIcon class="w-4 h-4 ml-1" />
                         </B24Button>
                       </div>
                     </div>
